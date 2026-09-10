d0b6cee6fc13689b1f1f3f5276ba1eba0a844a4a
rbansal617 <rahulbans400@gmail.com>
2026-09-10 14:28:31 -0400

    Refactor test_interset_ordinariness_uniqueness function

--- src/sentropy/tests/base_tests/sentropy_test.py
+++ src/sentropy/tests/base_tests/sentropy_test.py
@@ -412,29 +412,36 @@ def test_interset_ordinariness():
     expected = np.array([0.7375, 0.2625, 0.7   ])
     assert np.allclose(ordinariness, expected)
 def test_interset_ordinariness_uniqueness():
-    # Three objects in X whose ordinariness we want to calculate.
     X = np.array([
-        [1.0, 0.0],
-        [1.0, 0.0],
-        [0.7, 0.7],
+        [0],
+        [1],
+        [0],
+        [2],
     ])
 
-    # Four objects in the reference set Y.
     Y = np.array([
-        [1.0, 0.0],
-        [0.9, 0.1],
-        [0.9, 0.1],
-        [0.1, 0.9],
+        [0],
+        [1],
+        [0], 
+        [2],
+        [1],
     ])
 
-    # Raw counts for the four elements of Y.
-    Y_counts = np.array([100, 50, 25, 25])
-
-    ordinariness = interset_ordinariness(
+    Y_abundance = np.array([2, 1, 3, 4, 2,])
+    def similarity_function(x,y):
+        if x==y:
+            return 1
+        else:
+            return 0
+    result = interset_ordinariness(
         X=X,
         Y=Y,
-        Y_abundance=Y_counts,
-        similarity=lambda x,y : np.dot(x,y),
+        Y_abundance=Y_abundance,
+        similarity=similarity_function,
+        backend="numpy",
+        device="cpu",
     )
-    expected = np.array([0.9625, 0.9625, 0.7])
-    assert np.allclose(ordinariness, expected)
+
+    expected = np.array([5/12, 3/12, 5/12, 4/12])
+
+    assert np.allclose(result, expected)
